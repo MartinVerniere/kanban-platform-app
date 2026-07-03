0b1e7de37146d16ce1ff616ab5c923969af03903
MartinVerniere <mn.verniere@gmail.com>
2026-07-03 16:58:29 -0300

    Now requests are intercepted and attached with authToken (if exists)

---
+++ frontend/src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { AuthService } from '../auth-service';
+import { inject } from '@angular/core';
+
+export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<unknown>, next: (req: HttpRequest<unknown>) => any) => {
+	const authService = inject(AuthService);
+	const authToken: string | null = authService.getToken();
+
+	if (authToken) {
+		const authReq: HttpRequest<unknown> = request.clone({
+			setHeaders: {
+				Authorization: `Bearer ${authToken}`,
+			},
+		});
+		return next(authReq);
+	}
+
+	return next(request);
+};
