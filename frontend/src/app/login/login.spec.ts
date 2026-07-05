b497e1c85ae57f994015c217001114726a3d4cec
MartinVerniere <mn.verniere@gmail.com>
2026-07-05 19:38:55 -0300

    Added tests for login.ts

--- frontend/src/app/login/login.spec.ts
+++ frontend/src/app/login/login.spec.ts
@@ -1,22 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Login } from './login';
+import { AuthService } from '../services/auth-service';
+import { provideRouter, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('Login', () => {
-  let component: Login;
-  let fixture: ComponentFixture<Login>;
+	let fixture: ComponentFixture<Login>;
+	let component: Login;
+	let router: Router;
+	let navigateSpy: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Login],
-    }).compileComponents();
+	let authServiceMock: { login: ReturnType<typeof vi.fn> }; // Injected service
 
-    fixture = TestBed.createComponent(Login);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+	beforeEach(async () => {
+		authServiceMock = { login: vi.fn() };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		await TestBed.configureTestingModule({
+			imports: [Login],
+			providers: [
+				{ provide: AuthService, useValue: authServiceMock },
+				provideRouter([])
+			]
+		}).compileComponents();
+
+		router = TestBed.inject(Router);
+		navigateSpy = vi.spyOn(router, 'navigate');
+
+		fixture = TestBed.createComponent(Login);
+		component = fixture.componentInstance;
+
+		await fixture.whenStable();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should login on submit', () => {
+		authServiceMock.login.mockReturnValue(of({
+			user: {
+				id: '1',
+				username: 'john',
+				email: 'john@test.com'
+			}
+		}));
+
+		component.loginModel.set({
+			username: 'john',
+			password: '123',
+		});
+
+		component.onSubmit(new Event('submit'));
+
+		expect(authServiceMock.login).toHaveBeenCalledWith({
+			username: 'john',
+			password: '123',
+		});
+
+		expect(navigateSpy).toHaveBeenCalledWith(['/']);
+	});
+
+	it('should not submit when form is invalid', () => {
+		component.loginModel.set({
+			username: '',
+			password: '',
+		});
+
+		component.onSubmit(new Event('submit'));
+
+		expect(authServiceMock.login).not.toHaveBeenCalled();
+	});
 });
