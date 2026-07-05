a5eff366f71708bd91d5728a9beea30b46c93b96
MartinVerniere <mn.verniere@gmail.com>
2026-07-05 20:12:31 -0300

    Added tests for home.ts

--- frontend/src/app/home/home.spec.ts
+++ frontend/src/app/home/home.spec.ts
@@ -1,22 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Home } from './home';
+import { AuthService } from '../services/auth-service';
+import { provideRouter } from '@angular/router';
 
 describe('Home', () => {
-  let component: Home;
-  let fixture: ComponentFixture<Home>;
+	let fixture: ComponentFixture<Home>;
+	let component: Home;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Home],
-    }).compileComponents();
+	let authServiceMock: { // Injected service 
+		isLoggedIn: ReturnType<typeof vi.fn>,
+		login: ReturnType<typeof vi.fn>
+	};
 
-    fixture = TestBed.createComponent(Home);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+	beforeEach(async () => {
+		authServiceMock = {
+			login: vi.fn(),
+			isLoggedIn: vi.fn(),
+		};
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		await TestBed.configureTestingModule({
+			imports: [Home],
+			providers: [
+				{ provide: AuthService, useValue: authServiceMock },
+				provideRouter([])
+			],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(Home);
+		component = fixture.componentInstance;
+
+		await fixture.whenStable();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
