9c1380dcf464c222c016a1266d33c290eca37a48
MartinVerniere <mn.verniere@gmail.com>
2026-07-10 17:30:48 -0300

    Added button to create new project

--- frontend/src/app/project-list/project-list.ts
+++ frontend/src/app/project-list/project-list.ts
@@ -2,10 +2,11 @@ import { Component, inject, resource } from '@angular/core';
 import { ProjectsService } from '../services/projects-service';
 import { ProjectElement } from '../project-element/project-element';
 import { firstValueFrom } from 'rxjs';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-project-list',
-	imports: [ProjectElement],
+	imports: [ProjectElement, RouterLink],
 	templateUrl: './project-list.html',
 	styleUrl: './project-list.css',
 })
