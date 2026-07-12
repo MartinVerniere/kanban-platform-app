d6b05d518576e408b394d43d09799af1ba5b83d4
MartinVerniere <mn.verniere@gmail.com>
2026-07-12 19:27:00 -0300

    Added unit tests for project-form component

--- frontend/src/app/project-form/project-form.spec.ts
+++ frontend/src/app/project-form/project-form.spec.ts
@@ -1,22 +1,78 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectForm } from './project-form';
+import { Router, provideRouter } from '@angular/router';
+import { ProjectService } from '../services/project-service';
+import { of } from 'rxjs';
 
 describe('ProjectForm', () => {
-  let component: ProjectForm;
-  let fixture: ComponentFixture<ProjectForm>;
+	let fixture: ComponentFixture<ProjectForm>;
+	let component: ProjectForm;
+	let html: HTMLElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProjectForm],
-    }).compileComponents();
+	let projectServiceMock: { createProject: ReturnType<typeof vi.fn> }
 
-    fixture = TestBed.createComponent(ProjectForm);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+	async function createComponent(shouldAwait: boolean = true) {
+		fixture = TestBed.createComponent(ProjectForm);
+		component = fixture.componentInstance;
+		html = fixture.nativeElement;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture.detectChanges();
+
+		if (shouldAwait) {
+			await fixture.whenStable();
+			fixture.detectChanges();
+		}
+	}
+
+	beforeEach(async () => {
+		projectServiceMock = { createProject: vi.fn() };
+
+		await TestBed.configureTestingModule({
+			imports: [ProjectForm],
+			providers: [
+				{ provide: ProjectService, useValue: projectServiceMock },
+				provideRouter([])
+			]
+		}).compileComponents();
+	});
+
+	it('should create', async () => {
+		await createComponent();
+
+		expect(component).toBeTruthy();
+	});
+
+	it('should create project when valid form data, then redirect to /projects and clear form', async () => {
+		projectServiceMock.createProject.mockReturnValue(of({}));
+
+		await createComponent();
+
+		const router = TestBed.inject(Router);
+		const navigateSpy = vi.spyOn(router, 'navigate');
+
+		component.projectModel.set({
+			name: 'Project A',
+			key: 'PRO',
+			description: ''
+		});
+
+		await component.onSubmit(new Event('submit'));
+
+		expect(projectServiceMock.createProject).toHaveBeenCalledWith({ name: 'Project A', key: 'PRO', description: '' });
+		expect(navigateSpy).toHaveBeenCalledWith(['/projects']);
+		expect(component.projectModel()).toEqual({ name: '', key: '', description: '' });
+	});
+
+	it('should not create project when invalid form data', async () => {
+		await createComponent();
+
+		const form = html.querySelector('form')!;
+
+		form.dispatchEvent(new Event('submit'));
+
+		await fixture.whenStable();
+
+		expect(projectServiceMock.createProject).not.toHaveBeenCalled();
+	});
 });
