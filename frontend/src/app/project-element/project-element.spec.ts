3c3b878353fbaf5c003f9e7c7662a70286832a29
MartinVerniere <mn.verniere@gmail.com>
2026-07-12 20:48:49 -0300

    Added unit tests for project-element component

--- frontend/src/app/project-element/project-element.spec.ts
+++ frontend/src/app/project-element/project-element.spec.ts
@@ -1,22 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectElement } from './project-element';
+import { provideRouter } from '@angular/router';
+import { ProjectService } from '../services/project-service';
 
 describe('ProjectElement', () => {
-  let component: ProjectElement;
-  let fixture: ComponentFixture<ProjectElement>;
+	let fixture: ComponentFixture<ProjectElement>;
+	let component: ProjectElement;
+	let html: HTMLElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProjectElement],
-    }).compileComponents();
+	const projectServiceMock = {
+		getProjects: vi.fn(),
+		deleteProject: vi.fn()
+	};
 
-    fixture = TestBed.createComponent(ProjectElement);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+	const project = {
+		id: 1,
+		name: 'Project A',
+		key: 'PRA',
+		description: 'My project'
+	};
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	async function createComponent(shouldAwait: boolean = true) {
+		fixture = TestBed.createComponent(ProjectElement);
+		component = fixture.componentInstance;
+		html = fixture.nativeElement;
+
+		fixture.componentRef.setInput('project', project);
+
+		fixture.detectChanges();
+
+		if (shouldAwait) {
+			await fixture.whenStable();
+			fixture.detectChanges();
+		}
+	}
+
+	beforeEach(async () => {
+		vi.clearAllMocks();
+
+		await TestBed.configureTestingModule({
+			imports: [ProjectElement],
+			providers: [
+				{
+					provide: ProjectService,
+					useValue: projectServiceMock
+				},
+				provideRouter([])
+			]
+		}).compileComponents();
+	});
+
+	it('should create', async () => {
+		await createComponent();
+
+		expect(component).toBeTruthy();
+	});
+
+	it('should render project information', async () => {
+		await createComponent();
+
+		expect(html.textContent).toContain('Project A');
+		expect(html.textContent).toContain('PRA');
+		expect(html.textContent).toContain('My project');
+	});
 });
