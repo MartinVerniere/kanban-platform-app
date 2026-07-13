3958cff3653c7ead59f439c2e58837f9e5e1b4d4
MartinVerniere <mn.verniere@gmail.com>
2026-07-12 22:45:06 -0300

    Added unit tests for user-service service

--- frontend/src/app/services/user-service.spec.ts
+++ frontend/src/app/services/user-service.spec.ts
@@ -1,15 +1,68 @@
 import { TestBed } from '@angular/core/testing';
 import { UserService } from './user-service';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+
+const userA = {
+	id: 1,
+	username: 'john',
+	email: 'john@email.com',
+}
+
+const userB = {
+	id: 2,
+	username: 'alice',
+	email: 'alice@email.com',
+}
 
 describe('UserService', () => {
-  let service: UserService;
+	let service: UserService;
+	let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
-  });
+	beforeEach(() => {
+		vi.clearAllMocks();
+		localStorage.clear();
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+		TestBed.configureTestingModule({
+			providers: [
+				provideHttpClient(),
+				provideHttpClientTesting(),
+			]
+		});
+
+		httpMock = TestBed.inject(HttpTestingController);
+		service = TestBed.inject(UserService);
+	});
+
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
+
+	it('should get users', () => {
+		const expectedResponse = [userA, userB];
+
+		service.getUsers().subscribe(users => { expect(users).toEqual(expectedResponse); });
+
+		const request = httpMock.expectOne('http://localhost:3000/api/users');
+
+		expect(request.request.method).toBe('GET');
+
+		request.flush(expectedResponse);
+	});
+
+	it('should get user by id', () => {
+		const expectedResponse = userA;
+
+		service.getUser(1).subscribe(user => { expect(user).toEqual(expectedResponse); });
+
+		const request = httpMock.expectOne('http://localhost:3000/api/users/1');
+
+		expect(request.request.method).toBe('GET');
+
+		request.flush(expectedResponse);
+	});
+
+	afterEach(() => {
+		httpMock.verify();
+	});
 });
