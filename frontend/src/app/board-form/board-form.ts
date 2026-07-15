6ba21daa4d79f29185dd22edf7c0c0048ee9c783
MartinVerniere <mn.verniere@gmail.com>
2026-07-15 19:48:38 -0300

    Created board-form component

---
+++ frontend/src/app/board-form/board-form.ts
@@ -0,0 +1,65 @@
+import { Component, inject, input, output, signal } from '@angular/core';
+import { form, FormField, required, submit } from '@angular/forms/signals';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Board } from '../services/board-service';
+import { ProjectService } from '../services/project-service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+export interface BoardModel {
+	name: string;
+}
+
+@Component({
+	selector: 'app-board-form',
+	imports: [FormField],
+	templateUrl: './board-form.html',
+	styleUrl: './board-form.css',
+})
+export class BoardForm {
+	router = inject(Router);
+	projectService = inject(ProjectService);
+	route = inject(ActivatedRoute);
+
+	projectId = Number(this.route.snapshot.paramMap.get('projectId'));
+	boardAdded = output<void>();
+	canceledBboardAdded = output<void>();
+
+	boardModel = signal<BoardModel>({
+		name: ''
+	});
+	error = signal<string | null>(null);
+
+	boardForm = form(this.boardModel, (fieldPath) => {
+		required(fieldPath.name, { message: 'name is required' });
+	});
+
+	async onSubmit(event: Event) {
+		event.preventDefault();
+
+		submit(this.boardForm, async () => {
+			if (this.boardModel().name === '') return;
+
+			this.projectService.createBoard(this.projectId, this.boardModel()).subscribe({
+				next: () => {
+					this.resetForm();
+					this.error.set(null);
+					this.router.navigate(['/projects']);
+				},
+				error: (response: HttpErrorResponse) => {
+					const errorObject = response.error.error;
+					console.log(errorObject);
+					this.error.set(errorObject.message);
+				}
+			});
+		});
+	}
+
+	onCancel() {
+		this.router.navigate(['/projects']);
+	}
+
+	resetForm() {
+		this.boardModel.set({ name: '' });
+		this.boardForm().reset();
+	}
+}
