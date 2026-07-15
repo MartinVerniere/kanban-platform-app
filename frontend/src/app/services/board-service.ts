40d2de90a9615e208f016e90b008423fb9d4c26f
MartinVerniere <mn.verniere@gmail.com>
2026-07-15 19:15:21 -0300

    Created board service to udpate or delete a board by id

---
+++ frontend/src/app/services/board-service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:3000/api/boards';
+
+export interface Board {
+	id: number,
+	name: string,
+}
+
+@Service()
+export class BoardService {
+	private http = inject(HttpClient);
+
+	updateBoard(boardId: number, request: Board): Observable<Board> {
+		return this.http.put<Board>(`${API_URL}/boards/${boardId}`, request);
+	}
+
+	deleteBoard(boardId: number): Observable<void> {
+		return this.http.delete<void>(`${API_URL}/boards/${boardId}`);
+	}
+}
